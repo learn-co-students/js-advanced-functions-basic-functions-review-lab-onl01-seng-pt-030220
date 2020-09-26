81cb31b6ea06a4f4faf6424fc90e50d8231b6181
Haloren <mckennakyler@gmail.com>
2020-09-25 19:16:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(whatever = '*') {
+    let innerFunction = function(adj = 'special'){
+        return `You are ${whatever}${adj}${whatever}!`;
+    };
+    return innerFunction;
+}
+wrapAdjective("%")();
+
+let Calculator = {
+    add: (x, y) => {
+        return x + y;
+    },
+    subtract: (x, y) => {
+        return x - y;
+    },
+    multiply: (x,y) => {
+        return x*y;
+    },
+    divide: (x, y) => {
+        return x/y;
+    }
+};
+
+function actionApplyer (int, arr) {
+    for (let i = 0; i < arr.length; i++ ){
+        int = arr[i](int)
+      }
+      return int
+};
